de7fd69f9a60dceaf4be15a10bb813ee13739994
Ruslan Gusseinov <r.gusseinov@mail.ru>
2020-03-21 16:03:00 +0600

    Added quiz

---
+++ src/containers/ActiveQuiz/ActiveQuiz.jsx
@@ -0,0 +1,20 @@
+import React, {Component} from "react";
+
+const ActiveQuiz = props => (
+    <div>
+        <p>
+            <span> <strong> 1.</strong> &nbsp;
+             Как дела?
+            </span>
+            <small>4 из 12 </small>
+        </p>
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+            <li>4</li>
+        </ul>
+    </div>
+);
+
+export default ActiveQuiz;
